c25a78e609eb64a3a46c3a077b880e0ca7cd2a0f
luoruofeng <lrf8933@163.com>
2020-02-17 22:05:55 +0800

    shell finish

---
+++ news_scrapy/newspider/init.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+yum install openssl-devel bzip2-devel expat-devel gdbm-devel readline-devel sqlite-devel gcc gcc-c++ openssl-devel
+
+pip install --upgrade pip
+pip install html2text
+pip install scrapy
+pip install pymongo
+
+systemctl start mongod
+systemctl start crond
+
+`chmod 774 schedule`
+`crontab schedule`
+`crontab -l`
