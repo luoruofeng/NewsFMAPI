4bd18d165ef5dd19506c4c2201a22ab952860858
luoruofeng <lrf8933@163.com>
2020-02-22 21:24:17 +0800

    don't test baidu voice code commit

--- news_scrapy/newspider/main.py
+++ news_scrapy/newspider/main.py
@@ -8,9 +8,12 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == "__main__":
     #初始化拉取以前的文章
-    p_restart = subprocess.Popen(['scrapy', 'crawl', 'first_huxiu'])
+    # p_restart = subprocess.Popen(['scrapy', 'crawl', 'first_huxiu'])
+    app_path = os.path.dirname(os.path.realpath(__file__))
+    subprocess.Popen("scrapy crawl first_huxiu", shell=True, cwd=app_path)
 
     while(True):
         #每两小时拉取一次  www.huxiu/article
         time.sleep(60*60*2)
-        p_restart = subprocess.Popen(['scrapy', 'crawl', 'huxiu'])
+        # p_restart = subprocess.Popen(['scrapy', 'crawl', 'huxiu'])
+        subprocess.Popen("scrapy crawl huxiu", shell=True, cwd=app_path)
