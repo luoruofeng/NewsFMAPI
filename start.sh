c348f16233c03e1838088c2f07c7e31054634b85
luoruofeng <717750878@qq.com>
2020-02-23 11:18:51 +0800

    change start.sh on linux

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
 #/bin/bash
 
 #启动百度转化项目
+echo -e "\n执行baidu api进程\n"
 `chmod 774 ./voice/main.py`
 `nohup python ./voice/main.py  </dev/null 1>>./baidu.out 2>>./baidu.out &`
 
 #启动爬虫项目
-`chmod 774 ./news_scrapy/newspider/main.py`
-`nohup python ./news_scrapy/newspider/main.py </dev/null 1>>./scrapy.out 2>>./scrapy.out &`
+cd $(pwd)/news_scrapy/newspider
+echo -e "进入$(pwd)\n"
+echo -e "运行scrapy项目\n"
+`chmod 774 ./main.py`
+`nohup python ./main.py </dev/null 1>>$(pwd)/../../scrapy.out 2>>$(pwd)/../../scrapy.out &`
+cd $(pwd)/../../
+echo -e "回到目录$(pwd)\n"
